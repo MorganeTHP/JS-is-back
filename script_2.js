623d92f8a4869325ef2ad825c30626487675a7d7
Misa7 <ma.ducros7@gmail.com>
2019-02-21 22:59:35 +0100

    v Misa

--- script_2.js
+++ script_2.js
@@ -1,9 +1,11 @@
 function factorial(n) {
-	let nb = 1
-	while(n !== 0){
+	let nb = 1;
+	if (n == 0 || n == 1)
+		return 1;
+	while(n !== 0) {
 		nb *= n--
 	}
 	return nb
 }
-let userInput = prompt("Donnes-moi un nombre")
+let userInput = prompt("De quel nombre veux-tu calculer la factorielle ?")
 console.log(`Le résultat est : ${factorial(userInput)}`);
